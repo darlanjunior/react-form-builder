873d9f486c33177b26c20b09eb2e0d22a9ab4caf
Darlan Junior <darlan.sbjr@gmail.com>
2017-10-02 13:42:09 -0300

    Update package.json, remove react-sortable-items

--- src/preview.jsx
+++ src/preview.jsx
@@ -3,7 +3,6 @@
   */
 
 import React from 'react';
-import Sortable from 'react-sortable-items';
 import ElementStore from './stores/ElementStore';
 import ElementActions from './actions/ElementActions';
 import {Header,Paragraph,Label,LineBreak,TextInput,NumberInput,TextArea,Dropdown,Checkboxes,DatePicker,RadioButtons,Image,Rating,Tags,Signature,HyperLink,Download,Camera,Range} from './form-elements';
@@ -84,56 +83,22 @@ export default class Preview extends React.Component {
     let classes = this.props.className;
     if (this.props.editMode) { classes += ' is-editing'; }
     let items = this.state.data.map( item => {
-      switch (item.element) {
-        case "Header":
-          return <Header mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Paragraph":
-          return <Paragraph mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Label":
-          return <Label mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "LineBreak":
-          return <LineBreak mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "TextInput":
-          return <TextInput mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "NumberInput":
-          return <NumberInput mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "TextArea":
-          return <TextArea mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Dropdown":
-          return <Dropdown mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Checkboxes":
-          return <Checkboxes mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "DatePicker":
-          return <DatePicker mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "RadioButtons":
-          return <RadioButtons mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Rating":
-          return <Rating mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Image":
-          return <Image mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Tags":
-          return <Tags mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Signature":
-          return <Signature
-            mutable={false}
-            parent={this.props.parent}
-            editModeOn={this.props.editModeOn}
-            isDraggable={true}
-            key={item.id}
-            sortData={item.id}
-            data={item}
-            read_only={item.readOnly}
-            defaultValue={this.state.answer_data[item.field_name]}
-            _onDestroy={this._onDestroy} />
-        case "HyperLink":
-          return <HyperLink mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Download":
-          return <Download mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Camera":
-          return <Camera mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
-        case "Range":
-          return <Range mutable={false} parent={this.props.parent} editModeOn={this.props.editModeOn} isDraggable={true} key={item.id} sortData={item.id} data={item} _onDestroy={this._onDestroy} />
+      const itemProps = {
+        mutable: false,
+        parent: this.props.parent,
+        editModeOn: this.props.editModeOn,
+        isDraggable: true,
+        key: item.id,
+        sortData: item.id,
+        data: item,
+        _onDestroy: this._onDestroy
       }
+
+      return React.createElement(elementMap[item.element], itemProps)
+
+          // return <Signature {...itemProps} read_only={item.readOnly} defaultValue={this.state.answer_data[item.field_name]} />
+
+
     })
     return (
       <div className={classes}>
@@ -142,11 +107,35 @@ export default class Preview extends React.Component {
             <FormElementsEdit showCorrectColumn={this.props.showCorrectColumn} files={this.props.files} manualEditModeOff={this.props.manualEditModeOff} preview={this} element={this.props.editElement} updateElement={this.updateElement} />
           }
         </div>
-        <Sortable sensitivity={0} key={this.state.data.length} onSort={this.handleSort.bind(this)}>
+        {/* <Sortable sensitivity={0} key={this.state.data.length} onSort={this.handleSort.bind(this)}> */}
+        <div className="Sortable">
           {items}
-        </Sortable>
+        {/* </Sortable> */}
+        </div>
       </div>
     )
   }
+}
+
+const elementMap = {
+  "Header": Header,
+  "Paragraph": Paragraph,
+  "Label": Label,
+  "LineBreak": LineBreak,
+  "TextInput": TextInput,
+  "NumberInput": NumberInput,
+  "TextArea": TextArea,
+  "Dropdown": Dropdown,
+  "Checkboxes": Checkboxes,
+  "DatePicker": DatePicker,
+  "RadioButtons": RadioButtons,
+  "Rating": Rating,
+  "Image": Image,
+  "Tags": Tags,
+  "Signature": Signature,
+  "HyperLink": HyperLink,
+  "Download": Download,
+  "Camera": Camera,
+  "Range": Range
 }
 Preview.defaultProps = { showCorrectColumn: false, files: [], editMode: false, editElement: null, className: 'react-form-builder-preview pull-left'}
